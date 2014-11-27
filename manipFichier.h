5041a2f8e2085ffa465e18a997656a621fc85eda
Justin_Bridou <lonaxous.game@hotmail.fr>
2014-11-27 17:59:16 +0100

    Update manipFichier.h
    Déclaration des procédure pour inversement d'images.

--- manipFichier.h
+++ manipFichier.h
@@ -12,5 +12,9 @@ const int MAX = 150;
 // (vous devez utiliser ces sous-programmes, mais pas les modifier)
 void loadPicture(string picture, int red[][MAX], int green[][MAX], int blue[][MAX], int & taille);
 void writePicture(const int red[][MAX], const int  green[][MAX], const int  blue[][MAX], int taille, string name);
+//Procédure inversant l'image en fesant une sysmétrie verticale (SALVATO Lucas)
+void inversevert(int red[][MAX],int green[][MAX],int blue[][MAX],int taille);
+//Procédure inversant l'image en fesant une symétrie horizontale (SALVATO Lucas)
+void inversehor(int red[][MAX],int green[][MAX],int blue[][MAX],int taille);
 
 #endif
