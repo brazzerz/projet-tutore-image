cefe0e058ef74a6fa721626efe9fb75e637dcaed
Justin_Bridou <lonaxous.game@hotmail.fr>
2014-11-30 11:39:31 +0100

    Update Image.h

--- Image.h
+++ Image.h
@@ -15,6 +15,8 @@ public:
 	void inversehor();
 	void binarisation();
 	void negatif();
+	void rotation90();
+	void melimelo();//Bonus
 	void symetriecentrale();//Bonus
 	void write(string output);
 };
