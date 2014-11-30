c896e8a28612aad8b25eea0dc484e2e040a248d8
Justin_Bridou <lonaxous.game@hotmail.fr>
2014-11-30 11:47:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
 	cout<<"Entrez le nom du fichier de sortie"<<endl;
 	cin>>output;
 	int choix;
-	cout<<"1\tPresence rouge\n2\tSupprimer rouge\n3\tNegatif\n4\tNoir et blanc\n5\tLuminosite\n6\tSymetrie vertical\n7\tSymetrie horizontal\n8\tRotation\n9\tFlou\n10\tSymetrie centrale\n11\tMeliMelo\nChoix : ";
+	cout<<"1\tPresence rouge\n2\tSupprimer rouge\n3\tNegatif\n4\tNoir et blanc\n5\tLuminosite\n6\tSymetrie vertical\n7\tSymetrie horizontal\n8\tRotation\n9\tFlou\n10\tSymetrie centrale\n11\tMeliMelo\n12\tRotation a 270\nChoix : ";
 	cin>>choix;
 	switch(choix){
 		case 1:
@@ -28,6 +28,7 @@ int main(int argc, char const *argv[])
 		case 9:
 		case 10: image.symetriecentrale();image.write(output);break;
 		case 11: image.melimelo();image.write(output);break;
+		case 12: image.rotation270();image.write(output);break;
 		default: cout<<"Choix invalide."<<endl;break;
 	}
 	return 0;
